36a60cc7add405428294e512a41bf03ae16f3b02
Marc <marcgallagher16@gmail.com>
2019-04-30 22:01:22 +0100

    Shape

---
+++ Shape_Rotation/README.md
@@ -0,0 +1,5 @@
+# Drawing multiple circles on a grid system of tiles with rotation
+##Using HSB colours relative to mouse co-ordinates
+
+[See code in action](https://mgn00150905.github.io/GenerativeDesignProjects/Shape_Rotation) (Ctrl/Cmnd Click to open in new tab)
+<img src="circles.png" width="800" height="600"/>
--- Shape_Rotation/sketch02.js
+++ Shape_Rotation/sketch02.js
@@ -1,24 +1,26 @@
 
-let numTiles = 3;
+let numTiles = 6;
 let tileWidth;
 
 function setup(){
-  createCanvas(500,500);
+  createCanvas(windowWidth,windowHeight);
   tileWidth = width/numTiles;
   angleMode(DEGREES);
   background(0);
+  colorMode(HSB);
 }
 
 function draw(){
   for(let gridY = 0; gridY < numTiles; gridY++){
     for(let gridX = 0; gridX < numTiles; gridX++){
-      rectMode(CENTER);
+      // rectMode(CENTER);
       strokeWeight(1);
-      fill(255);
       push();
       translate(gridX*tileWidth+tileWidth/2, gridY*tileWidth+tileWidth/2);
-      rotate(mouseY);
-      rect(0, 0, tileWidth, tileWidth);
+      rotate(mouseX);
+      scale(1/3);
+      fill(mouseX/2, mouseY/2, mouseX);
+      ellipse(-tileWidth/2, tileWidth, tileWidth, 50);
       pop();
 
     }
